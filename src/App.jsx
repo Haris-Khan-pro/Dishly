60ddf6b79aac62dd0cbcc74d6e1946000c04840c
Haris Khan <hariskhan01.gsm@gmail.com>
2026-09-09 14:51:33 +0500

    first commit and some defaulf setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./section/HeroSection";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection/>
+    </>
+  );
+};
+
+export default App;
