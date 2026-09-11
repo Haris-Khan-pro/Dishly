055a74bfc3fa111aab93ad75da53f93b27557629
Haris Khan <hariskhan01.gsm@gmail.com>
2026-09-11 17:56:52 +0500

    Add FAQs section with animated FAQ items and create CTA section

---
+++ src/sections/CTA.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CTA = () => {
+  return (
+    <section
+      id="cta"
+      className="relative overflow-hidden bg-orange-500 px-6 flex flex-col justify-center items-center min-h-100 md:min-h-110 mt-44"
+    ></section>
+  );
+};
+
+export default CTA;
