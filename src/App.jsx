716365dd10b2bbe6f97ed7e83c536711b6dd02e3
Haris Khan <hariskhan01.gsm@gmail.com>
2026-09-11 18:24:44 +0500

    Add CTA section with animated dishes and booking call-to-action

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import BookingProcess from "./sections/BookingProcess";
 import Timing from "./sections/Timing";
 import TestimonialSection from "./sections/TestimonialSection";
 import FAQs from "./sections/FAQs";
+import CTA from "./sections/CTA";
 
 const App = () => {
   return (
@@ -22,7 +23,8 @@ const App = () => {
       <BookingProcess />
       <Timing />
       <TestimonialSection />
-      <FAQs/>
+      <FAQs />
+      <CTA />
     </>
   );
 };
